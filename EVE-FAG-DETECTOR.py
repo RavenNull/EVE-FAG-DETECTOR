573adb3834176cbe0548254f5a02c409da4ec599
Raven Null <contact@ravennull.com>
2016-03-29 15:20:05 -0500

    Update EVE-FAG-DETECTOR.py

--- EVE-FAG-DETECTOR.py
+++ EVE-FAG-DETECTOR.py
@@ -1,8 +1,9 @@
+import re
 print("EVE FAG DETECTOR v2.2.3")
 eveName = input("What is the character's name?")
 if(eveName == "Raven Null"):
 	print("Raven Null is not a fag.")
-elif(eveName == "raven null"):
+elif(eveName == m = re.match(user_input, "raven null", re.IGNORECASE);
 	print("Learn how to use the shift key dipshit. Also, no faggotry was detected for Raven Null.")
 else:
 	print("Player is confirmed to be a fag.")
